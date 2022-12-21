56718ece38c55c12d0e925b9905d38c88aae338b
faderz <58748338+faderzz@users.noreply.github.com>
2022-12-21 14:55:45 +0000

    yes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import { CSSTransition } from 'react-transition-group';
+import ImageButton from './components/redpill.js';
 
 function App() {
+  const [showButton, setShowButton] = useState(true);
+  const [showDiv, setShowDiv] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div style={{textAlign: 'center'}}>
+        <ImageButton onClick={() => window.location.href='/home'}/>
+      </div>
   );
 }
 
---
+++ src/components/redpill.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ImageButton() {
+  return (
+    <button onClick={() => window.location.href='/'}>
+      <img src="https://0t.fyi/rCka5y/direct" alt="Button" />
+    </button>
+  );
+}
+
+export default ImageButton;
